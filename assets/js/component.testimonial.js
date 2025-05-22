ec2529f0df2af6bc01e4b2bce5c98da9963b07f3
Rework-Digital <liam@reworkdigital.com.au>
2025-05-22 11:31:58 +1000

    updated timing

--- assets/js/component.testimonial.js
+++ assets/js/component.testimonial.js
@@ -2,19 +2,19 @@
 export default function initTestimonial() {
   console.log('[testimonial] init');
   new Splide('#slider_testimonial', {
-    type       : 'slide',   // or 'loop' if you want it to wrap
-    perPage    : 3,
-    perMove    : 1,
-    gap        : '22px',
-    speed      : 600,
-    autoplay   : true,
-    interval   : 2000,
-    pauseOnHover : true,
-    pauseOnFocus : true,
-    pagination : false,
-    drag       : 'free',
-    snap       : true,
-    trimSpace  : true,
+    type          : 'loop',   // or 'loop' if you want it to wrap
+    perPage       : 3,
+    perMove       : 1,
+    gap           : '22px',
+    speed         : 600,
+    autoplay      : true,
+    interval      : 8000,
+    pauseOnHover  : true,
+    pauseOnFocus  : true,
+    pagination    : false,
+    drag          : 'free',
+    snap          : true,
+    trimSpace     : true,
     breakpoints: {
       991: { perPage: 2, gap: '22px' },
       767: { perPage: 1, gap: '16px' },
