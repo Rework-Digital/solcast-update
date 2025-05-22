0ca6e10fdd57a99bed8e0ccf4a9d158715540322
Rework-Digital <liam@reworkdigital.com.au>
2025-05-22 10:58:49 +1000

    Update loader

--- assets/js/component.loader.js
+++ assets/js/component.loader.js
@@ -5,8 +5,14 @@
   document.addEventListener('DOMContentLoaded', () => {
     // Only import & run the init script if the carousel container exists
     if (document.querySelector('#slider_logoCarousel')) {
-      import('./component.logoCarousel.js');
-    }
+      import('./component.logoCarousel.js')
+      .then(module => {
+      // assume module.default or module.initLogoCarousel runs the init
+      module.default?.();
+      console.log('[loader] logoCarousel module loaded');
+    })
+    .catch(err => console.error('[loader] failed to load logoCarousel:', err));
+}
   });
 
 
