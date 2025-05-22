17ffc214a282fe7355db1f2c58ff24f9ce3295d8
Rework-Digital <liam@reworkdigital.com.au>
2025-05-22 10:23:38 +1000

    Update settings

--- assets/js/component.testimonial.js
+++ assets/js/component.testimonial.js
@@ -7,23 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (splideElement) {
     new Splide('#splide__testimonial', {
-      type: 'slide',
-      drag: 'free',
-      gap: '2rem',
       perPage: 3,
       perMove: 1,
-      snap: true,
-      autoWidth: true,
+      focus: 0, // 0 = left and 'center' = center
+      type: 'slide', // 'loop' or 'slide'
+      gap: '22px', // space between slides
+      arrows: slider, // 'slider' or false
+      pagination: false, // 'slider' or false
+      speed : 600, // transition speed in miliseconds
+      dragAngleThreshold: 30, // default is 30
+      autoWidth: false, // for cards with differing widths
+      rewind : false, // go back to beginning when reach end
+      waitForTransition : false,
+      updateOnMove : true,
+      trimSpace: true, // true removes empty space from end of list
       autoHeight: false,
-      autoplay: true,
-      interval: 2000,
-      pagination: true,
-      pauseOnHover: true,
-      pauseOnFocus: true,
-      focusableNodes: 'img',
-      reducedMotion: {
-        speed: 0,
-        autoplay: 'pause',
+      breakpoints: {
+        991: {
+          // Tablet
+          perPage: 2.5,
+          gap: '22px',
+        },
+        767: {
+          // Mobile Landscape
+          perPage: 1.5,
+          gap: '16px',
+        },
+        479: {
+          // Mobile Portrait
+          perPage: 1,
+          gap: '12px',
+        }
       }
     }).mount(window.splide.Extensions);
   }
