6634e341aca7a167ba16bf2a248e9735ecabc81d
Rework-Digital <liam@reworkdigital.com.au>
2025-05-22 10:38:21 +1000

    testing

--- assets/js/component.loader.js
+++ assets/js/component.loader.js
@@ -2,9 +2,33 @@
 /* Splide Loader */
 /*---------------*/
 
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   // Only import & run the init script if the carousel container exists
+  //   if (document.querySelector('#slider_logocarousel')) {
+  //     import('./component.logoCarousel.js');
+  //   }
+  // });
+
+
   document.addEventListener('DOMContentLoaded', () => {
-    // Only import & run the init script if the carousel container exists
-    if (document.querySelector('#slider_logocarousel')) {
-      import('./component.logoCarousel.js');
-    }
-  });
+  const splideElement = document.querySelector('.splide');
+
+  if (splideElement) {
+    new Splide('.splide', {
+      type: 'loop',
+      drag: 'free',
+      gap: '2rem',
+      autoWidth: true,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      focusableNodes: 'img',
+      autoScroll: {
+        speed: 0.8,
+      },
+      reducedMotion: {
+        speed: 0,
+        autoplay: 'pause',
+      }
+    }).mount(window.splide.Extensions);
+  }
+});
