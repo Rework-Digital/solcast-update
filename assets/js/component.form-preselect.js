9d561ff10102745e314f609bce6d5d771b022eb9
Rework-Digital <liam@reworkdigital.com.au>
2025-07-09 10:40:19 +1000

    update the preselect script

--- assets/js/component.form-preselect.js
+++ assets/js/component.form-preselect.js
@@ -1,19 +1,26 @@
 export default function initProductPreselect() {
   const productSelect = document.querySelector('select[data-preselect]');
 
-  if (!productSelect) return;
+  if (!productSelect) {
+    console.warn('[formPreselect] No select element found with data-preselect attribute.');
+    return;
+  }
 
   const preselectValue = productSelect.getAttribute('data-preselect');
 
-  if (preselectValue) {
-    const optionExists = Array.from(productSelect.options).some(
-      (opt) => opt.value === preselectValue
-    );
+  if (!preselectValue) {
+    console.warn('[formPreselect] data-preselect attribute is empty.');
+    return;
+  }
 
-    if (optionExists) {
-      productSelect.value = preselectValue;
-    } else {
-      console.warn(`[productSelect] No matching option for: "${preselectValue}"`);
-    }
+  const matchingOption = Array.from(productSelect.options).find(
+    (opt) => opt.value === preselectValue
+  );
+
+  if (matchingOption) {
+    productSelect.value = preselectValue;
+    console.log(`[formPreselect] Preselected product: "${preselectValue}"`);
+  } else {
+    console.warn(`[formPreselect] No matching option for value: "${preselectValue}"`);
   }
 }
