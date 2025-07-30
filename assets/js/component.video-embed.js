dc10b231e3a17f9f6663a787104eb4fac7671385
Rework-Digital <liam@reworkdigital.com.au>
2025-07-30 16:10:08 +1000

    Update video script

--- assets/js/component.video-embed.js
+++ assets/js/component.video-embed.js
@@ -1,20 +1,24 @@
-	// Select the parent div with the class 'section-banner-component-video_wrapper'
-  const videoWrapper = document.querySelector('.section-banner-component-video_wrapper');
-    
-  // Get the div that contains the data-video-src and data-object-fit attributes
-  const videoDiv = videoWrapper.querySelector('[data-video-src]');
-  
+// Select all parent divs with the class 'section-banner-component-video_wrapper'
+const videoWrappers = document.querySelectorAll('.section-banner-component-video_wrapper');
+
+videoWrappers.forEach(wrapper => {
+  // Get the div inside each wrapper that contains the data-video-src and data-object-fit attributes
+  const videoDiv = wrapper.querySelector('[data-video-src]');
+  if (!videoDiv) return; // Skip if no videoDiv is found
+
   // Get the value of the data-video-src attribute
   const videoSrc = videoDiv.getAttribute('data-video-src');
-    
+
   // Get the value of the data-object-fit attribute
   const objectFit = videoDiv.getAttribute('data-object-fit');
-   
+
   // Get the video element within that div
   const videoElement = videoDiv.querySelector('video');
-    
+  if (!videoElement) return; // Skip if no video element is found
+
   // Update the src attribute of the video element with the new video source
   videoElement.setAttribute('src', videoSrc);
-    
-  // Update the object-fit style property of the video element with the new value from data-object-fit
-  videoElement.style.objectFit = objectFit;
+
+  // Update the object-fit style property of the video element
+  if (objectFit) videoElement.style.objectFit = objectFit;
+});
