42b297d83851767a39619474a3a952820b65ba31
Rework-Digital <liam@reworkdigital.com.au>
2025-08-06 11:46:33 +1000

    update form preselect

--- assets/js/component.form-preselect.js
+++ assets/js/component.form-preselect.js
@@ -1,41 +1,33 @@
-export default function initProductPreselect() {
+export default function initFormPreselects() {
+  // ****** Product Preselect ****** //
   const productSelect = document.querySelector('select[data-preselect]');
-  if (!productSelect) return;
-
-  const preselectValue = productSelect.getAttribute('data-preselect');
-  if (!preselectValue) return;
-
-  const matchingOption = Array.from(productSelect.options).find(
-    (opt) => opt.value === preselectValue
-  );
-
-  if (matchingOption) {
-    productSelect.value = preselectValue;
-  }
-}
-
-
-// ****** Preselects USA in Forms ****** //
-
-export default function preselectCountryUSA() {
-  // Get all forms on the page
-  const forms = document.querySelectorAll('form');
-  if (!forms.length) return;
-
-  forms.forEach(form => {
-    // Find all selects with ID containing "country" (case-insensitive)
-    const selects = form.querySelectorAll('select[id*="country" i]');
-    selects.forEach(select => {
-      // Find the option that matches "United States of America"
-      const usaOption = Array.from(select.options).find(
-        opt => opt.text.trim().toLowerCase() === 'united states of america'
+  if (productSelect) {
+    const preselectValue = productSelect.getAttribute('data-preselect');
+    if (preselectValue) {
+      const matchingOption = Array.from(productSelect.options).find(
+        opt => opt.value === preselectValue
       );
-
-      if (usaOption) {
-        select.value = usaOption.value;
-        // Trigger change event in case any listeners are attached
-        select.dispatchEvent(new Event('change', { bubbles: true }));
+      if (matchingOption) {
+        productSelect.value = preselectValue;
       }
+    }
+  }
+
+  // ****** Preselects USA in Forms ****** //
+  const forms = document.querySelectorAll('form');
+  if (forms.length) {
+    forms.forEach(form => {
+      const selects = form.querySelectorAll('select[id*="country" i]');
+      selects.forEach(select => {
+        const usaOption = Array.from(select.options).find(
+          opt => opt.text.trim().toLowerCase() === 'united states of america'
+        );
+
+        if (usaOption) {
+          select.value = usaOption.value;
+          select.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+      });
     });
-  });
+  }
 }
